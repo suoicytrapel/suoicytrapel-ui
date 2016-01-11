26f08896cab7ce74f095a06c05a8798baf13f573
Mohit <mohitsingla2256@gmail.com>
2016-01-11 20:10:22 +0530

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leparticiousUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the leparticiousUiApp
+ */
+angular.module('leparticiousUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leparticiousUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the leparticiousUiApp
+ */
+angular.module('leparticiousUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
