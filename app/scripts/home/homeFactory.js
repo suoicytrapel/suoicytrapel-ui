52f8386226c5c83e6acbce7425ffe96bb8167d57
Mohit <mohitsingla2256@gmail.com>
2016-01-13 23:30:44 +0530

    Correcting indentation

--- app/scripts/home/homeFactory.js
+++ app/scripts/home/homeFactory.js
@@ -6,24 +6,24 @@ var app = angular.module("leparticiousUiApp");
 app.factory('HomeFactory', function ($resource) {
    
     return $resource('http://localhost:8080/api/rest/v1/user/:id', {id: "@id" }, {
-            saveUser: { 
-            	method: "POST"
-            },
-             updateUser: { 
-            	method: "PUT"
-            },
-             deleteUser: { 
-            	method: "DELETE"
-            },
-             getUser: { 
-            	method: "GET",
-            	isArray: false,
-        		transformResponse: function(data, header) {
-  					return angular.fromJson(data);
-        		}
-            },
-             getAllUsers: { 
-            	method: "GET"
-            }
-        });
+        saveUser: { 
+        	method: "POST"
+        },
+         updateUser: { 
+        	method: "PUT"
+        },
+         deleteUser: { 
+        	method: "DELETE"
+        },
+         getUser: { 
+        	method: "GET",
+        	isArray: false,
+    		transformResponse: function(data, header) {
+					return angular.fromJson(data);
+    		}
+        },
+         getAllUsers: { 
+        	method: "GET"
+        }
+    });
 });
