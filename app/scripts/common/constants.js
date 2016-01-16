4dbd7a323a0bec08547eee19b8b4909771b2f077
Mohit <mohitsingla2256@gmail.com>
2016-01-17 00:51:29 +0530

    Created Constant file

--- app/scripts/common/constants.js
+++ app/scripts/common/constants.js
@@ -2,8 +2,6 @@ var app = angular.module("leparticiousUiApp");
 
 
 //inject the value in the controller using its name "defaultInput"
-app.service('homeService', function() {
-   
-   
-
+app.constant('Constants', {
+	API_HOST: 'http://localhost:8080/api/rest/v1'
 });
