0e359a01d7c372c4ab49d52849db43005a181e6d
Ankit Bansal <anku171@gmail.com>
2016-02-09 22:59:39 +0530

    committing maps module

--- app/scripts/detail/detailController.js
+++ app/scripts/detail/detailController.js
@@ -1,9 +1,11 @@
-app.controller('detailController', function($scope, $rootScope, baseFactory, $timeout, $location, HomeFactory, HomeService, dataService, DataFactory) {
+app.controller('detailController', function($scope, $rootScope, $interval, baseFactory, $timeout, $location, HomeFactory, HomeService, dataService, DataFactory) {
 	var vm = this;
 	vm.init = function(){
 		$rootScope.showCover = false;
 		vm.selectedCategory = baseFactory.getSelectedCategory();
 		vm.name = dataService.getName();
+		//google.maps.event.addDomListener(window, 'load', loadMap(28.012496, 73.336364));
+		checkForMaps();
 	};
 	vm.defineListeners =  function(){
 		vm.openGallery = function(){
@@ -24,6 +26,32 @@ app.controller('detailController', function($scope, $rootScope, baseFactory, $ti
 					console.log(error);
 				});
 	};
+	
+	function checkForMaps(){
+		var interval = setInterval(function(){
+			if (typeof google === 'object' && typeof google.maps === 'object') {
+        loadMap();
+        clearInterval(interval);
+    } 
+  },100);
+	}
+	
+	function loadMap() {
+    	var myCenter = new google.maps.LatLng(28.012496, 73.336364);
+    	var mapProp = {
+        	center: myCenter,
+        	zoom: 8,
+        	mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(angular.element('#map')[0], mapProp);
+
+    var marker = new google.maps.Marker({
+        position: myCenter,
+    });
+
+    marker.setMap(map);
+}
+
 	
 	vm.init();
 	vm.fetchDetails();
