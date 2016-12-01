4b1c21327dcd152550eae4f8c57a09c5fd5bfcc3
Ankit Bansal <anku171@gmail.com>
2016-12-01 07:39:21 +0530

    checking in forgot reset password functionality

--- app/scripts/login/resetforgotpwdController.js
+++ app/scripts/login/resetforgotpwdController.js
@@ -15,6 +15,32 @@ app.controller('resetforgotpwdController', function($scope, ModalService, close,
 	 	$element.modal('hide');
 	 	close();
 	 	};
+	 vm.showSignInPopup = function(msg) {
+		vm.closePopup();
+		ModalService.showModal({
+			templateUrl : "/views/login/signin.html",
+			controller : "loginController",
+			controllerAs : "vm",
+		}).then(function(modal) {
+			/* Opening a modal via javascript */
+			modal.element.modal();
+			
+			if(msg){
+			modal.controller.messageType = msg.type;
+			modal.controller.messageBarMessage = msg.message;
+			}
+			/* returning a promise on closing a modal */
+			modal.close.then(function(result) {
+				console.log(result);
+			});
+			/* when closing modal on clicking outside modal area
+			 * the modal element should be removed form DOM */
+			modal.element.on('hidden.bs.modal', function () {
+            modal.controller.closePopup();
+        });
+		});
+	};	
+	 	
 	 vm.sendDetails = function(){
 	 	if($scope.form.resetforgotpwdForm.$valid){
 	 		if(LoginFactory.getResetForgotPass().decodedUsername){
@@ -22,10 +48,10 @@ app.controller('resetforgotpwdController', function($scope, ModalService, close,
 	 			username: LoginFactory.getResetForgotPass().decodedUsername,
 	 			password: vm.resetforgotpwdForm.newPassword,
 	 		};
-	 		var promise = LoginFactory.resetForgotPassword().send(resetForgotPwdParams).$promise;
+	 		var promise = LoginFactory.resetForgotPassword.send(resetForgotPwdParams).$promise;
 	 		
 	 		promise.then(function(data){
-	 			$scope.showSignInPopup({type: 'Success', message: 'Message: Reset Password Successful, Please login using new password'});
+	 			vm.showSignInPopup({type: 'Success', message: 'Message: Reset Password Successful, Please login using new password'});
 	 		},function(error){
 	 			vm.messageType = 'Error';
 				vm.messageBarMessage = 'Error Message: Unable to reset password, Please try again after sometime';
